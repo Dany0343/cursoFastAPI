756e2c3e71d4e08df81b532b226e6c7c56363e17
Oscar Bucio <oscarbucio2001@gmail.com>
2022-11-28 02:02:29 -0600

    edit: main.py to make a hello world with FastAPI

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI # Esta clase permite que todo el framework funcione
+
+app = FastAPI() # Esta variable va a contener a toda la aplicacion, se instancia de la clase FastAPI y se crea un objeto de tipo FastAPI
+
+# Path Operations
+@app.get("/") # Path Operation Decorator, va a decorar a una función que se creará posteriormente Este decordador está utilizando el método get que viene del objeto app que a su vez es una instancia de FastAPI
+
+def home(): # El primer lugar donde un usuario de la API va a aparecer cuando entre a la misma 
+    return {"Hello": "World"} # Se retorna un JSON
