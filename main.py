965befde05b29fddc1b4a438a5911fc0075ecc71
Oscar Bucio <oscarbucio2001@gmail.com>
2022-11-29 23:12:49 -0600

    add: createPerson request on main.py

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ app = FastAPI() # Esta variable va a contener a toda la aplicacion, se instancia
 
 def home(): # El primer lugar donde un usuario de la API va a aparecer cuando entre a la misma 
     return {"Hello": "Worldd"} # Se retorna un JSON
+
+
+# Request and Response
+# Se va a probar como se envía un Request Body a una API, como el cliente se comunica con el servidor, se hace un request, se hace una petición de tipo POST
+@app.post("/person/new") # Se tiene un endpoint
+def createPerson():
+    return
